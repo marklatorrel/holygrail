05f6f57d2e168860352c9eeaddd1c3172fb54d03
Mark La Torre <mlatorre@pucp.pe>
2021-09-14 15:49:37 -0500

    finished holy grail

---
+++ key_get_set.js
@@ -0,0 +1,10 @@
+var redis = require("redis");
+var client = redis.createClient();
+
+client.mset('header',0,'left',0,'article',0,'right',0,'footer',0);
+client.mget(['header', 'left', 'article', 'right', 'footer'], 
+function(err, value){
+    console.log(value);
+});
+
+client.quit();
